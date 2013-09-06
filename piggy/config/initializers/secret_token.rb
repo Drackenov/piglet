a723af2346603a14688644b359d94f387676b94f
Drake Talley <robertdraketalley@gmail.com>
2013-09-06 00:36:40 +0000

    rack app

---
+++ piggy/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Piggy::Application.config.secret_key_base = '562c8e3523c1f34f46601031428c216206453e3507d6755669d6dd2ec0e9613fe524b0d7d26b23ab3c3c6f7e3fd8b9e474d29909f2f2ab3e7df9eca6f98747b2'
